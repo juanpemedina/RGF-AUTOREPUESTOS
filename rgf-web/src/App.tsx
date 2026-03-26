ae4d6c1acfc0f8d0c6c300b2fb37c94fc326929b
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2026-03-26 16:00:20 -0600

    initial commit

---
+++ rgf-web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="font-body">
+      <Navbar />    
+    </div>
+  );
+}
+
+export default App
---
+++ rgf-web/src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center px-6 py-4 shadow">
+      <h1 className="font-title text-xl text-primary">
+        RGF Autorepuestos
+      </h1>
+
+      <ul className="flex gap-6">
+        <li><a href="#about">Nosotros</a></li>
+        
+        {/* Botón oculto */}
+        <li className="hidden">
+          <a href="#catalog">Catálogo</a>
+        </li>
+
+        <li>
+          <a href="#contact" className="text-secondary font-semibold">
+            Contacto
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
